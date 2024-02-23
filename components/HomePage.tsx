4fef2f820b861dff7392e56420505355463c532f
edakaraman <95571155+edakaraman@users.noreply.github.com>
2024-02-23 14:10:19 +0300

    Home Page
    Since it is a single-page application, only the Home page was created.

---
+++ components/HomePage.tsx
@@ -0,0 +1,11 @@
+"use client"
+import React from 'react'
+import MyForm from '../components/MyForm'
+
+export default function HomePage() {
+  return (
+    <div>
+        <MyForm/>
+    </div>
+  )
+}
